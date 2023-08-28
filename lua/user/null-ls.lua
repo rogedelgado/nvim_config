08550e5b86f5f1d53f9b8f6284022925f7d2c144
Rogelio Delgado <rogelio@ssf.mx>
2023-08-28 13:49:42 -0600

    Remove the spell completion from the nvim-cmp source

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -4,7 +4,7 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
+        null_ls.builtins.completion.luasnip,
         null_ls.builtins.diagnostics.flake8.with({extra_args= {"--ignore=E501"}}),
         null_ls.builtins.formatting.isort,
         null_ls.builtins.code_actions.refactoring,

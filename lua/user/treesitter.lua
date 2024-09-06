651192c96703d7b3cb6d77dbb673d24062f7c374
Rogelio Delgado <rogelio@ssf.mx>
2024-09-05 19:17:12 -0600

    Enable only the treesitter modules that I use

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,14 +1,27 @@
 local configs = require("nvim-treesitter.configs")
-configs.setup {
-  ensure_installed = "all",
-  sync_install = false,
-  ignore_install = { "" }, -- List of parsers to ignore installing
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-
-  }
-  -- ,
-  --indent = { enable = true, disable = { "yaml" } },
-}
+configs.setup({
+	ensure_installed = {
+		"bash",
+		"java",
+		"python",
+		"sql",
+		"diff",
+		"lua",
+		"html",
+		"markdown",
+		"markdown_inline",
+		"query",
+		"vim",
+		"vimdoc",
+        "yaml"
+	},
+	sync_install = false,
+	ignore_install = { "" }, -- List of parsers to ignore installing
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "" }, -- list of language that will be disabled
+		additional_vim_regex_highlighting = true,
+	},
+	-- ,
+	--indent = { enable = true, disable = { "yaml" } },
+})

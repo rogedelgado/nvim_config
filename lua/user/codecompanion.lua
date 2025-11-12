808889ca41f78f38f7c89dc6f029219b5e90464b
Rogelio Delgado <rogelio@ssf.mx>
2025-11-11 22:12:49 -0600

    Avoid codecompanion adapters API deprecation

--- lua/user/codecompanion.lua
+++ lua/user/codecompanion.lua
@@ -1,13 +1,15 @@
 require("codecompanion").setup({
-    adapters = {
-        gemini = function()
-            return require("codecompanion.adapters").extend("gemini", {
-                env = {
-                    api_key = "cmd:cat /home/roge/.gemini",
-                }
-            })
-        end,
-    },
+	adapters = {
+		http = {
+			gemini = function()
+				return require("codecompanion.adapters").extend("gemini", {
+					env = {
+						api_key = "cmd:cat /home/roge/.gemini",
+					},
+				})
+			end,
+		},
+	},
 	strategies = {
 		chat = {
 			adapter = "gemini",
